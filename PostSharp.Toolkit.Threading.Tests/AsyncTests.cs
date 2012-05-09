16a718b7562d943391a093fba58a1580fa315776
skwasiborski <Slawomir.Kwasiborski@internetium.pl>
2012-05-09 14:09:04 +0200

    Tests for ReaderWriterSynchronized, SingleThreaded.

---
+++ PostSharp.Toolkit.Threading.Tests/AsyncTests.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AsyncTests.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Diagnostics;
+using System.Threading;
+
+using NUnit.Framework;
+
+using Threading;
+
+namespace PostSharp.Toolkit.Threading.Tests
+{
+    [TestFixture]
+    public class AsyncTests
+    {
+        [Test]
+        public void WhenAsyncCalledInvokesAsynchronously()
+        {
+            var a = new AsyncEntity();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            a.Async(100);
+            stopwatch.Stop();
+            Assert.Less(stopwatch.ElapsedMilliseconds, 50);
+        }
+    }
+
+    public class AsyncEntity
+    {
+        [Async]
+        public void Async(int timespan)
+        {
+            Thread.Sleep(timespan);
+        }
+    }
+}
