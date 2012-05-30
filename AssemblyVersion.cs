7b2a28f17f3671d457f145f8dbe60d841faa6387
gfraiteur <gael@sharpcrafters.com>
2012-05-30 10:57:18 +0200

    NuGet build.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,4 +1,2 @@
 [assembly: System.Reflection.AssemblyVersion( "2.1.0.0" )]
-#if !SMALL
-[assembly: System.Reflection.AssemblyFileVersion( "2.1.1.4" )]
-#endif
+[assembly: System.Reflection.AssemblyFileVersion( "2.1.1.5" )]
