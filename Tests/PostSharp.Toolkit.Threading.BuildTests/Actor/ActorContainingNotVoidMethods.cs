8f1bdb0aa441620f5943b146cba6e9214e9b19f8
skwasiborski <Slawomir.Kwasiborski@internetium.pl>
2012-06-04 18:02:50 +0200

    Tests for actor build validation added

---
+++ Tests/PostSharp.Toolkit.Threading.BuildTests/Actor/ActorContainingNotVoidMethods.cs
@@ -0,0 +1,25 @@
+﻿// @ExpectedMessage(THR009)
+// @ExpectedMessage(PS0060)
+namespace PostSharp.Toolkit.Threading.BuildTests.Actor
+{
+    namespace ActorContainingNotVoidMethods
+    {
+        class Program
+        {
+            public static int Main()
+            {
+                return 0;
+            }
+
+           
+        }
+
+        class ActorClass : PostSharp.Toolkit.Threading.Actor
+        {
+            public int StaticThreadUnsafe()
+            {
+                return 1;
+            }
+        }
+    }
+}
