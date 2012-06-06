e514dac80a80fbe859ecc1fcc6021af244a7b080
gfraiteur <gael@sharpcrafters.com>
2012-06-06 13:37:22 +0200

    Actor: added support for SynchronizationContext.

---
+++ Threading/PostSharp.Toolkit.Threading/DispatcherSynchronizationContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PostSharp.Toolkit.Threading
+{
+    public sealed class DispatcherSynchronizationContext : SynchronizationContext
+    {
+        private readonly IDispatcher dispatcher;
+
+        public DispatcherSynchronizationContext( IDispatcher dispatcher )
+        {
+            this.dispatcher = dispatcher;
+        }
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            this.dispatcher.BeginInvoke( new SendOrPostCallbackAction( d, state ) );
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            if ( this.dispatcher.CheckAccess() )
+            {
+                d( state );
+            }
+            else
+            {
+                this.dispatcher.Invoke( new SendOrPostCallbackAction( d, state ) );
+            }
+        }
+
+        class SendOrPostCallbackAction : IAction
+        {
+            private readonly SendOrPostCallback callback;
+            private readonly object state;
+
+
+            public SendOrPostCallbackAction( SendOrPostCallback callback, object state )
+            {
+                this.callback = callback;
+                this.state = state;
+            }
+
+            public void Invoke()
+            {
+                callback(this.state);
+            }
+        }
+
+    }
+}
