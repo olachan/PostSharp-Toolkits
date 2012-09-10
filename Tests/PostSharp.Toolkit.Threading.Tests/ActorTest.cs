cd64b80d40ddb352e39920413e7e17347f4db702
Sławomir Kwasiborski <slawomir.kwasiborski@internetium.pl>
2012-09-10 12:27:42 +0200

    Actor test fix

--- Tests/PostSharp.Toolkit.Threading.Tests/ActorTest.cs
+++ Tests/PostSharp.Toolkit.Threading.Tests/ActorTest.cs
@@ -38,7 +38,7 @@ namespace PostSharp.Toolkit.Threading.Tests
         public void TestFast()
         {
             const int n = 1000000;
-            ActorClass[] actors = new ActorClass[Environment.ProcessorCount - 1];
+            ActorClass[] actors = new ActorClass[Math.Max(Environment.ProcessorCount - 1, 1)];
 
             for (int i = 0; i < actors.Length; i++)
             {
