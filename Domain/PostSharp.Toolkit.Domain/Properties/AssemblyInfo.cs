146017228dc84efbf229d73c6d97621dc5e24919
Sławomir Kwasiborski <slawomir.kwasiborski@internetium.pl>
2012-09-12 13:52:08 +0200

    Updated Domain and Threading AssemblyInfo files

--- Domain/PostSharp.Toolkit.Domain/Properties/AssemblyInfo.cs
+++ Domain/PostSharp.Toolkit.Domain/Properties/AssemblyInfo.cs
@@ -17,11 +17,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle( "PostSharp.Toolkit.Domain" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "PostSharp.Toolkit.Domain" )]
-[assembly: AssemblyCopyright( "Copyright ©  2012" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,17 +27,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid( "deb8647b-8809-4d45-b4af-d8af73feedad" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
