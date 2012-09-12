11ad9d9b9a31a132d4a1d6065f1a3a1fd9f45206
Sławomir Kwasiborski <slawomir.kwasiborski@internetium.pl>
2012-09-12 12:35:15 +0200

    Revision update

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,2 +1,2 @@
 [assembly: System.Reflection.AssemblyVersion( "2.1.0.0" )]
-[assembly: System.Reflection.AssemblyFileVersion( "2.1.1.10" )]
+[assembly: System.Reflection.AssemblyFileVersion( "2.1.1.11" )]
