04c90ca7e97fa1aa9e5b011d8b80854b140dcedc
Sławomir Kwasiborski <slawomir.kwasiborski@internetium.pl>
2012-09-25 12:27:54 +0200

    Added tests for logging involving abstract methods

---
+++ Tests/PostSharp.Toolkit.Tests/AbstractMethodsTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace PostSharp.Toolkit.Tests
+{
+    // ReSharper disable InconsistentNaming
+
+    [TestFixture]
+    public class AbstractMethodsTests : BaseTestsFixture
+    {
+        [Test]
+        public void LoggingToolkit_Methods_LogsMethodEnter()
+        {
+            DerivedFromLogAbstract s = new DerivedFromLogAbstract();
+            s.AbstractMethiod();
+
+            string output = OutputString.ToString();
+            StringAssert.Contains("Entering: PostSharp.Toolkit.Tests.DerivedFromLogAbstract.AbstractMethiod()", output);
+        }
+    }
+
+    // ReSharper restore InconsistentNaming 
+
+    public abstract class LogWithAbstract
+    {
+        public abstract void AbstractMethiod();
+    }
+
+    public class DerivedFromLogAbstract : LogWithAbstract
+    {
+        public override void AbstractMethiod()
+        {
+
+        }
+    }
+}
